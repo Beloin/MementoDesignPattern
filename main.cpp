56c6121862bb8643459418d4ec0589eb23b4083c
Beloin Sena <beloin.rodrigues@gmail.com>
2022-05-29 12:54:05 -0300

    Identified Holders

--- main.cpp
+++ main.cpp
@@ -12,22 +12,28 @@ using namespace std;
 int main () {
     auto app = new Application();
     auto holder1 = app->createShapeHolder(new Point(12, 45), "Blue");
-    auto holder2 = app->createShapeHolder(new Point(45, 12), "Blue");
+    auto holder2 = app->createShapeHolder(new Point(45, 12), "Yellow");
 
+    cout << "1: ";
     holder1->show(cout);
-//    holder2->show(cout);
+    cout << "2: ";
+    holder2->show(cout);
 
     holder1->move(new Point(67, 12));
     holder2->move(new Point(55, 55));
 
+    cout << "1: ";
     holder1->show(cout);
-//    holder2->show(cout);
+    cout << "2: ";
+    holder2->show(cout);
 
     holder1->undo();
     holder2->undo();
 
+    cout << "1: ";
     holder1->show(cout);
-//    holder2->show(cout);
+    cout << "2: ";
+    holder2->show(cout);
 
     return 0;
 }
